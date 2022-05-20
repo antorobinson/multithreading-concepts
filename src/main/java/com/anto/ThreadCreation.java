7f58fea65c77ae6b78041545a098e1e0d2e0a011
Sahaya Anto Robinson <anto_robinson@outlook.com>
2022-05-20 23:39:14 +0530

    added comments

--- src/main/java/com/anto/ThreadCreation.java
+++ src/main/java/com/anto/ThreadCreation.java
@@ -4,12 +4,16 @@ public class ThreadCreation {
 
 	public static void main(String[] args) {
 
+		// new thread by extending Thread class
 		Thread thread = new MyThread();
 		thread.start();
 
+		// new thread by passing runnable object
 		Thread runnableThread = new Thread(new MyRunnableThread());
 		runnableThread.start();
 
+		// from java8, we can pass implementation of runnable 
+		// objects directly to Thread Constructor
 		Thread runnableThread2 = new Thread(
 				() -> System.out.println("Hi, I am from a lambda implemented runnbale thread"));
 		runnableThread2.start();

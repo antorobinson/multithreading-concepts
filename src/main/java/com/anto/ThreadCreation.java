e1d8bfec5d45dba5d6fb1bc068c868067dd83398
Sahaya Anto Robinson <anto_robinson@outlook.com>
2022-05-21 13:25:43 +0530

    updated runnable types

--- src/main/java/com/anto/ThreadCreation.java
+++ src/main/java/com/anto/ThreadCreation.java
@@ -4,6 +4,7 @@ public class ThreadCreation {
 
 	public static void main(String[] args) {
 
+		System.out.println(Thread.currentThread().getName()+" : Inside main ");
 		// new thread by extending Thread class
 		Thread thread = new MyThread();
 		thread.start();
@@ -11,29 +12,36 @@ public class ThreadCreation {
 		// new thread by passing runnable object
 		Thread runnableThread = new Thread(new MyRunnableThread());
 		runnableThread.start();
-
+		
+		// new thread by creating anonymous inner class of runnable interface
+		Runnable anonymousRunnable = new Runnable(){
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName()+" : Inside anonymous runnable ");
+			}
+		};
+		Thread runnableThread2 = new Thread(anonymousRunnable);
+		runnableThread2.start();
+		
 		// from java8, we can pass implementation of runnable 
 		// objects directly to Thread Constructor
-		Thread runnableThread2 = new Thread(
-				() -> System.out.println("Hi, I am from a lambda implemented runnbale thread"));
-		runnableThread2.start();
+		Thread runnableThread3 = new Thread(
+				() -> System.out.println(Thread.currentThread().getName()+ " : Inside lambda runnable"));
+		runnableThread3.start();
 	}
-
 }
 
 class MyThread extends Thread {
 
 	@Override
 	public void run() {
-		System.out.println("Hi, I am from a thread class");
+		System.out.println(Thread.currentThread().getName()+" : Inside thread class");
 	}
-
 }
 
 class MyRunnableThread implements Runnable {
 
 	public void run() {
-		System.out.println("Hi, I am from a runnable thread class");
+		System.out.println(Thread.currentThread().getName()+" : Inside runnable thread");
 	}
-
 }

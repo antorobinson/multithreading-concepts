f7332fbf9f66f92c29a1b316a4c7c3ff603b57cd
Sahaya Anto Robinson <anto_robinson@outlook.com>
2022-05-22 13:47:35 +0530

    updated synchronized counter

--- thread-synchronization-example/src/com/anto/SynchronizedCounter.java
+++ thread-synchronization-example/src/com/anto/SynchronizedCounter.java
@@ -3,12 +3,18 @@ package com.anto;
 public class SynchronizedCounter {
 	private int count;
 	
+	
+	/**
+	 * synchronized keyword allows the calling thread to gain the intrinsic lock of an object of this class. 
+	 * this intrinsic lock does not allow other threads to access any synchronized methods in the object.
+	 * however other threads can access non-synchronized methods.
+	 * 
+	 */
 	public synchronized void increment(){
 		System.out.println("Inside synchronized method");
 		try {
 			Thread.sleep(50);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		count += 1;
